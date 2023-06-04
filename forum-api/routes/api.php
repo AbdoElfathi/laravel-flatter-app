03aa1cae4ec2a57c50700e35afbe99229e84de19
Abdo <abdessamadelfathi2018@gmail.com>
2023-06-04 14:47:05 +0100

    first commit

---
+++ forum-api/routes/api.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\AuthenticationController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/test', function()
+{
+    return response([
+        'message' => "Api is working",
+    ], 200);
+});
+
+Route::post('register',[AuthenticationController::class, 'register'])->name('userRegister');
